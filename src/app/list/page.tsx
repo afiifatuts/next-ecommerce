bc4af1da0a9ccc8315d94cb5958bb184f8818c17
afiifatuts <afiifatuts04@gmail.com>
2025-01-21 23:10:21 +0700

    done: footer do:list

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,6 +1,21 @@
+import Image from "next/image"
+
 const ListPage = () => {
   return (
-    <div>ListPage</div>
+    <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
+      {/* CAMPAIGN */}
+      <div className="">
+        <div className=""></div>
+        <div className="relative">
+          <Image
+          src="/woman.png"
+          alt=""
+          fill
+          className="object-contain"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
